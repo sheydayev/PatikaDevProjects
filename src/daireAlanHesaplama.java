e77d8dcc8af9a4ac5522f7b2b0296fb06ee94c03
Senan Sheydayev <sanansheydayev@gmail.com>
2023-01-05 19:04:07 +0400

    05.01.2023

---
+++ src/daireAlanHesaplama.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class daireAlanHesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int r;
+        double pi = 3.14;
+        System.out.print("Dairenin yarı çapını giriniz: ");
+        r=input.nextInt();
+        double alan = pi *r *r;
+        double cevre = 2*pi*r;
+        System.out.println("Dairenin alanı: "+alan);
+        System.out.println("Dairenin çevresi: " +cevre);
+    }
+}
