4797ee0a32424e19c6084549f5516d0ea10ea4ab
Senan Sheydayev <sanansheydayev@gmail.com>
2023-01-12 20:07:02 +0400

    12.01.2023

--- src/havaSicaklik.java
+++ src/havaSicaklik.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class havaSicaklik {
 
     //Hava Sıcaklığına Göre Etkinlik Önerme
+    /*  Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+        Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+        Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
+        Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+        */
     public static void main(String[] args) {
         int hava;
         Scanner scan = new Scanner(System.in);
